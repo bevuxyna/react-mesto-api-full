159b6709972ffd3c98e98fde77fbeb407a79683e
bevuxyna <wannhorn@gmail.com>
2022-08-07 00:19:19 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,10 @@ const app = express();
 
 app.use(helmet());
 
+app.use(cors);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cors);
 
 app.use(requestLogger); // подключаем логгер запросов
 
