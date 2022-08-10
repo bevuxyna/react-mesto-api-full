ec1b85686e1ec407138aafb7d9b173b103dc7d79
bevuxyna <wannhorn@gmail.com>
2022-08-10 20:17:12 +0300

    fix validation password

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -29,7 +29,7 @@ module.exports.validateLogin = celebrate({
 module.exports.validateCreateUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
